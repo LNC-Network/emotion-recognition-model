d11c0edfd66c96e690830de214c86c07a549c06d
Jit Debnath <jit.nathdeb@gmail.com>
2025-08-05 22:22:13 +0530

    changes

--- test.py
+++
@@ -1,7 +0,0 @@
-from utility.loader import cnn_loader
-
-model, transformer, class_names, device = cnn_loader("models/mood_cnn.pth","data/test")
-
-from utility.camera import cv2ModelRunner
-
-cv2ModelRunner(model, class_names, transformer, device)
---
+++ video-capture_test.py
@@ -0,0 +1,7 @@
+from utility.loader import cnn_loader
+from utility.camera import cv2ModelRunner
+
+
+model, transformer, class_names, device = cnn_loader("models/model.pth","data/test")
+
+cv2ModelRunner(model, class_names, transformer, device)
